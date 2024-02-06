e536ab781668c71b29186ce256dcd14a4de0c6b3
Malin  Sjögren <mail2malins@gmail.com>
2024-02-06 18:55:23 +0000

    Add about page with admin access

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
 # Create your models here.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -27,13 +26,13 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
